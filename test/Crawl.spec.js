14fcec9549b84c3823bd8c76c8022ec18e3b3e52
Steven Morrison <stevenmorrimorrison@hotmail.co.uk>
2018-07-15 20:22:04 +0100

    SM: Added crawl tests

--- test/Crawl.spec.js
+++ test/Crawl.spec.js
@@ -1,10 +1,49 @@
-const Input = require('../src/Crawl.js');
+const Crawl = require('../src/Crawl.js');
+const Input = require('../src/Input.js');
 const expect =  require('chai').expect;
 
 describe('Crawl actions', () => {
 
-    // Test init()
+    /**
+     * getHostUrl test - remove proceeding 'https' or 'http'.
+     */
+    it('Should return host url (without \'HTTP\')', () => {
+        
+        let testUrl = 'http://test.com';
+        let testUrlMatch = 'test.com';
 
-    // Test getHostUrl method
+        let crawlTest = new Crawl(testUrl, {}, Input, {});
+        let result = crawlTest.getHostUrl(testUrl);
+
+        expect(result).to.equal(testUrlMatch);
+    });
+
+    /**
+     * getHostUrl test - remove proceeding 'https' or 'http'.
+     */
+    it('Should return host url (without \'HTTPS\')', () => {
+        
+        let testUrl = 'https://test.com';
+        let testUrlMatch = 'test.com';
+
+        let crawlTest = new Crawl(testUrl, {}, Input, {});
+        let result = crawlTest.getHostUrl(testUrl);
+
+        expect(result).to.equal(testUrlMatch);
+    });
+
+    /**
+     * getUrlPath test - return url path.
+     */
+    it('Should return url path', () => {
+        
+        let testUrl = 'https://test.com';
+        let testUrlPath = 'test.com/path1';
+
+        let crawlTest = new Crawl(testUrl, {}, Input, {});
+        let result = crawlTest.getUrlPath(testUrlPath);
+
+        expect(result).to.equal('/path1');
+    });
 
 });
